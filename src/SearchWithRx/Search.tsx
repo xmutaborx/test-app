2d6bf618b4ce0dcd2d61fedfe6ea7ccbb07e6fd9
Danil Ladygin <dladygin@ladygin.office.devexperts.com>
2020-02-21 12:10:59 +0300

    search github users with rxjs

--- src/SearchWithRx/Search.tsx
+++ src/SearchWithRx/Search.tsx
@@ -1,29 +1,91 @@
 import * as React from 'react';
 import {request$} from "./SearchService";
 import {ajax} from "rxjs/ajax"
-import {of, Subject, fromEvent, Subscriber, Subscription} from "rxjs";
-import {debounceTime, distinctUntilChanged, tap, map, switchMap, mergeMap} from "rxjs/operators";
+import {EMPTY, Subject, Subscription} from "rxjs";
+import {debounceTime, distinctUntilChanged, map, switchMap, mergeMap, catchError, tap, filter} from "rxjs/operators";
 
 export type TSearchState = {
-    inputValue: string
+    inputValue: string,
+    canIShow: boolean,
+    users: [{
+        login: string,
+        id: number,
+        node_id: string,
+        avatar_url: string,
+        gravatar_id: string,
+        url: string,
+        html_url: string,
+        followers_url: string,
+        following_url: string,
+        gists_url: string,
+        starred_url: string,
+        subscriptions_url: string,
+        organizations_url: string,
+        repos_url: string,
+        events_url: string,
+        received_events_url: string,
+        type: string,
+        site_admin: boolean,
+        score: number
+    }]
 }
 
 export class Search extends React.PureComponent<{}, TSearchState> {
-    readonly state: TSearchState = {inputValue: ''};
+    readonly state: TSearchState = {
+        inputValue: '',
+        canIShow: false,
+        users: [{
+            login: '',
+            id: 0,
+            node_id: '',
+            avatar_url: '',
+            gravatar_id: '',
+            url: '',
+            html_url: '',
+            followers_url: '',
+            following_url: '',
+            gists_url: '',
+            starred_url: '',
+            subscriptions_url: '',
+            organizations_url: '',
+            repos_url: '',
+            events_url: '',
+            received_events_url: '',
+            type: '',
+            site_admin: false,
+            score: 0
+        }]
+    };
+
     stream$ = new Subject();
     sub?: Subscription;
-    URL: string = `https://api.github.com/search/users?q=`;
 
     componentDidMount(): void {
+        const URL = `https://api.github.com/search/users?q=`;
         this.sub = this.stream$
             .pipe(
                 debounceTime(1000),
                 distinctUntilChanged(),
-                switchMap(v => ajax.getJSON(`${this.URL}${v}`)),
+                tap(v => {
+                   if (v === '') {
+                       this.setState({canIShow: false})
+                   }
+                }),
+                filter(v => v !== ''),
+                switchMap(v => ajax.getJSON(`${URL}${v}`)
+                    .pipe(
+                        catchError(() => EMPTY)
+                    )),
                 map((response: any) => response.items),
-                mergeMap(items => items)
+                tap(data => {
+                    this.setState({canIShow: true, users: data})
+                })
             )
             .subscribe(v => console.log(v))
+    }
+
+    componentWillUnmount(): void {
+        this.sub?.unsubscribe()
     }
 
     handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -32,7 +94,7 @@ export class Search extends React.PureComponent<{}, TSearchState> {
     };
 
     render() {
-        const {inputValue} = this.state;
+        const {inputValue, users, canIShow} = this.state;
 
         return (
             <>
@@ -42,6 +104,14 @@ export class Search extends React.PureComponent<{}, TSearchState> {
                         value={inputValue}
                         onChange={this.handleChangeInput}
                     />
+                </div>
+                <div className={"search__container"}>
+                    {users[0].id !== 0 && canIShow && users.map(item =>
+                        <div key={item.id}>
+                            <p>{item.login}</p>
+                            <img src={item.avatar_url} width={250}/>
+                        </div>
+                    )}
                 </div>
             </>
         )
